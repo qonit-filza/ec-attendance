2d6db7c1071c2cb515a4026955aa05f35866b5d4
qonit-filza <109884831+qonit-filza@users.noreply.github.com>
2023-02-15 16:41:14 +0700

    add gradient background

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
+import BottomNav from "./components/BottomNav.vue";
+import BackgroundOverlay from "./components/BackgroundOverlay.vue";
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <main>
+    <BackgroundOverlay />
+    <RouterView />
+  </main>
 </template>
---
+++ client/src/components/BackgroundOverlay.vue
@@ -0,0 +1,5 @@
+<template>
+  <div
+    class="w-screen h-screen bg-gradient-to-b from-blue-700 via-blue-400 to-gray-300 -z-10 fixed top-0 left-0"
+  ></div>
+</template>
