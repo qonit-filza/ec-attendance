2d40b6634cdd90e46554066a93376d58a5d04240
qonit-filza <109884831+qonit-filza@users.noreply.github.com>
2023-02-15 16:23:00 +0700

    create bottom nav component

---
+++ client/src/components/BottomNav.vue
@@ -0,0 +1,17 @@
+<script setup>
+import IconClock from "./icons/IconClock.vue";
+import IconCalendar from "./icons/IconCalendar.vue";
+import IconUser from "./icons/IconUser.vue";
+</script>
+
+<template>
+  <nav
+    class="bg-white fixed bottom-0 w-full h-14 rounded-t-xl border shadow-[0_-20px_25px_-5px_rgba(0,0,0,0.1)]"
+  >
+    <div class="flex flex-row justify-around mt-4">
+      <IconClock />
+      <IconCalendar />
+      <IconUser />
+    </div>
+  </nav>
+</template>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from "../components/TheWelcome.vue";
+import BottomNav from "../components/BottomNav.vue";
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <BottomNav />
   </main>
 </template>
