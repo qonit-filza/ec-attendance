f1450d9a72cc52af8a6085c882fb4265e1b37a5c
qonit-filza <109884831+qonit-filza@users.noreply.github.com>
2023-02-15 19:35:29 +0700

    add main card

--- client/src/views/AttendancePage.vue
+++ client/src/views/AttendancePage.vue
@@ -17,5 +17,27 @@ import BottomNav from "../components/BottomNav.vue";
         <p>Electronic City SCBD</p>
       </div>
     </div>
+
+    <div class="bg-white text-gray-700 text-center py-2 rounded-lg mt-28">
+      <p class="text-xs">Your Working Hours</p>
+      <p class="text-md font-semibold mt-1">09:00 - 18:00</p>
+    </div>
+
+    <div
+      class="bg-white text-gray-700 text-center py-4 rounded-lg mt-4 flex flex-row justify-around"
+    >
+      <div class="">
+        <p class="text-xs pt-1.5">Clock-in</p>
+        <p class="text-2xl font-semibold mt-1">08:30</p>
+      </div>
+      <div>
+        <p class="text-xs pt-1.5">Clock-out</p>
+        <p class="text-2xl font-semibold mt-1">--:--</p>
+      </div>
+      <div>
+        <p class="text-xs pt-1.5">Working</p>
+        <p class="text-2xl font-semibold mt-1">00:30</p>
+      </div>
+    </div>
   </section>
 </template>
