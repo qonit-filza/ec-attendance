300abffaba6eacc95b40463e1f2ec5a21775e7b2
qonit-filza <109884831+qonit-filza@users.noreply.github.com>
2023-02-16 00:47:52 +0700

    create profile card

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import LoginPage from "../views/LoginPage.vue";
 import AttendancePage from "../views/AttendancePage.vue";
 import ListAttendancePage from "../views/ListAttendancePage.vue";
+import ProfilePage from "../views/ProfilePage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,9 +23,9 @@ const router = createRouter({
       component: ListAttendancePage,
     },
     {
-      path: "/about",
-      name: "about",
-      component: () => import("../views/AboutView.vue"),
+      path: "/profile",
+      name: "profile",
+      component: ProfilePage,
     },
   ],
 });
---
+++ client/src/views/ProfilePage.vue
@@ -0,0 +1,48 @@
+<script setup>
+import TopBar from "../components/TopBar.vue";
+</script>
+
+<template>
+  <section>
+    <TopBar />
+
+    <div
+      class="bg-white text-gray-700 text-center py-2 rounded-lg mt-20 relative"
+    >
+      <img
+        class="w-20 h-20 rounded-full absolute -top-10 left-1/2 -translate-x-1/2"
+        src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+        alt="Rounded avatar"
+      />
+
+      <div class="my-14 flex flex-col gap-5">
+        <div>
+          <p class="text-xs">Name</p>
+          <p class="text-md font-semibold mt-1">John Doe</p>
+        </div>
+
+        <div>
+          <p class="text-xs">Job Title</p>
+          <p class="text-md font-semibold mt-1">Web Developer</p>
+        </div>
+
+        <div>
+          <p class="text-xs">Email</p>
+          <p class="text-md font-semibold mt-1">johndoe@mail.com</p>
+        </div>
+
+        <div>
+          <p class="text-xs">Phone</p>
+          <p class="text-md font-semibold mt-1">08123456789</p>
+        </div>
+
+        <div>
+          <p class="text-xs">Address</p>
+          <p class="text-md font-semibold mt-1 w-full text-center px-16">
+            Jl Kebon Kacang no. 10, Jakarta Pusat
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
