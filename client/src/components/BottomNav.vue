f0cc7373c8f44a9e621d8fd4256359d37cc46739
qonit-filza <109884831+qonit-filza@users.noreply.github.com>
2023-02-16 01:25:33 +0700

    add route nav on bottom nav

--- client/src/components/BottomNav.vue
+++ client/src/components/BottomNav.vue
@@ -9,9 +9,17 @@ import IconUser from "./icons/IconUser.vue";
     class="bg-white fixed bottom-0 w-full h-14 rounded-t-xl border shadow-[0_-20px_25px_-5px_rgba(0,0,0,0.1)]"
   >
     <div class="flex flex-row justify-around mt-4">
-      <IconClock />
-      <IconCalendar />
-      <IconUser />
+      <router-link to="/" active-class="text-[#EF9C38]">
+        <IconClock />
+      </router-link>
+
+      <router-link to="/attendance-records" active-class="text-[#EF9C38]">
+        <IconCalendar />
+      </router-link>
+
+      <router-link to="/profile" active-class="text-[#EF9C38]">
+        <IconUser />
+      </router-link>
     </div>
   </nav>
 </template>
--- client/src/views/LoginPage.vue
+++ client/src/views/LoginPage.vue
@@ -33,12 +33,7 @@ import CustomButton from "../components/CustomButton.vue";
         />
       </div>
 
-      <button
-        type="submit"
-        class="bg-[#EF9C38] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-md px-5 py-2.5 mr-2 mb-2 focus:outline-none w-full"
-      >
-        Log in
-      </button>
+      <CustomButton buttonType="submit" buttonText="Log in" />
     </form>
 
     <div class="text-gray-600 text-center fixed bottom-4 left-0 w-full text-sm">
