145ddb4e89f23d7b3c027f8ed8d07decc89d0be6
qonit-filza <109884831+qonit-filza@users.noreply.github.com>
2023-02-16 14:33:59 +0700

    fetch user on top bar

--- client/src/components/TopBar.vue
+++ client/src/components/TopBar.vue
@@ -1,14 +1,35 @@
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
+import axios from "axios";
+const userId = localStorage.getItem("userId");
+
+const user = reactive({
+  name: "",
+  profilePic: "",
+});
+fetchUser();
+
+async function fetchUser() {
+  try {
+    const { data } = await axios.get(`http://localhost:3000/users/${userId}`);
+    user.name = data.name;
+    user.profilePic = data.profilePic;
+  } catch (error) {
+    console.log(error);
+  }
+}
+</script>
 
 <template>
   <section class="flex flex-row justify-between">
     <div>
-      <p class="text-xs">Good Morning</p>
-      <p class="text-lg font-semibold">John Doe</p>
+      <p class="text-xs">Welcome back</p>
+      <p class="text-lg font-semibold">{{ user.name }}</p>
     </div>
     <img
+      v-if="user.profilePic"
       class="w-10 h-10 rounded-full"
-      src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+      :src="user.profilePic"
       alt="Rounded avatar"
     />
   </section>
